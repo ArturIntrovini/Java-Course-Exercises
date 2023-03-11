984a0045e644a6de98df2bf598a17038484f04db
Artur <47034969+ArturIntrovini@users.noreply.github.com>
2023-03-11 06:45:19 -0400

    Add files via upload

---
+++ Ch. 7: OOP Part 1 - Inheritance/Wall/Wall.java
@@ -0,0 +1,53 @@
+public class Wall {
+
+    private double width = 0;
+    private double height = 0;
+
+    public Wall() {
+        this.width = width;
+        if (width<0) {
+            width = 0;
+        }
+        this.height = height;
+        if (height<0) {
+            height = 0;
+        }
+    }
+
+    public Wall(double width, double height) {
+        if (width<0) {
+            width = 0;
+        }
+        this.width = width;
+        if (height<0) {
+            height = 0;
+        }
+        this.height = height;
+    }
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        if (width<0) {
+            width = 0;
+        }
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        if (height<0) {
+            height = 0;
+        }
+        this.height = height;
+    }
+
+    public double getArea() {
+        return (width*height);
+    }
+
+}
