d5b2e961800d412473a2bcaebfd6dfc1ff8db73b
Artur <47034969+ArturIntrovini@users.noreply.github.com>
2023-03-11 06:27:08 -0400

    Add files via upload

--- Ch. 4: IntelliJ Basics/Main.java
+++ Ch. 4: IntelliJ Basics/Main.java
@@ -2,24 +2,19 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int myVariable = 50;
+        boolean gameOver = true;
+        int score = 900;
+        int levelCompleted = 5;
+        int bonus = 100;
 
-        myVariable++;
-        myVariable--;
-
-        System.out.println("This is a test");
-
-        System.out.println("This is" +
-                " another" +
-                " still more");
-
-        int anotherVariable = 50;
-        myVariable--;
-        System.out.println("myVariable =" + myVariable);
-
-        if (myVariable == 0) {
-            System.out.println("It's now zero");
+        if (score < 5000 && score > 1000) {
+            System.out.println("Your score was less than 5000 but greater than 1000");
+        } else if (score < 1000) {
+            System.out.println("Your score was less than 1000 ");
+        } else {
+            System.out.println("Got here");
         }
 
     }
+
 }
